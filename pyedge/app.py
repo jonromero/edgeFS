5ca3ddd9a5533d896f09076bd5bce5d50f9c6f3f
Jon Vlachogiannis <darksun4@gmail.com>
2015-10-25 21:31:06 -0700

    python lib

---
+++ pyedge/app.py
@@ -0,0 +1,81 @@
+from flask import Flask, request, jsonify
+import sys
+import requests
+app = Flask(__name__)
+
+edgeNodes = {'127.0.0.1:5005': 99,
+             '127.0.0.1:8000': 93}
+
+edgeIndex = [{'README.txt': [{'ip': '127.0.0.1:5005',
+                              'ip': '127.0.0.1:5008'}]},
+              {'.gitignore': [{'ip': '32.233.44.56:8000'}]}
+                               
+             ]
+
+@app.route('/')
+def index():
+    return jsonify(index=edgeIndex,
+                   nodes=edgeNodes)
+
+
+@app.route('/connect')
+def connect():
+    global edgeNodes
+    remote_ip = request.remote_addr
+    # if new connection
+    if remote_ip not in edgeNodes.keys():
+        edgeNodes[remote_ip] = 50
+        
+    return jsonify(nodes=edgeNodes)
+
+def connect_to_edge(host):
+    global edgeIndex
+    response = requests.get(host + "/connect")
+    print response
+    edgeIndex = response.json()['nodes']
+
+
+
+@app.route('/search/<filename>')
+def search(filename):
+    global nodes
+    with open(filename) as fd:
+        return fd.read()
+    return "nok"    
+
+def search_for_file(filename):
+    urls = [node[filename] for node in nodes]
+    for url in urls:
+        requests.get(url + "/search/" + filename)
+
+
+
+@app.route('/store/<filename>')
+def store(filename, methods=['PUT']):
+    f = request.files['the_file']
+    f.save(filename)
+
+@app.route('/dist-store/<filename>')
+def dist_store(filename, methods=['PUT']):
+    global nodes
+    urls = [node['score'] for node in nodes]
+    requests.put(urls[0] + "/store/" + filename, data=request.files['the_file'])
+
+
+def store_file(filename):
+    edge_url = None
+    with open(filename, "rb") as fd:
+        requests.put(edge_url + "/store/" + filename, data=fd.read())
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print "starting as Edge master - but why do you want to do that?"
+        app.run(debug=True, port=5000)
+        
+    else:
+        print "connecting to", sys.argv[1]
+        connect_to_edge('http://'+sys.argv[1])
+        app.run(debug=True, port=5001)
+    
